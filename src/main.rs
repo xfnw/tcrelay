0a34814c9a691ac92a040be78ec84791a37c8007
xfnw <xfnw@riseup.net>
2024-02-17 16:59:02 -0500

    pass state through service

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use clap::Parser;
 use http_body_util::Full;
 use hyper::{body::Bytes, server::conn::http1, service::service_fn, Request, Response};
 use hyper_util::rt::TokioIo;
-use std::{error::Error, net::SocketAddr};
+use std::{
+    error::Error,
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+};
 use tokio::net::TcpListener;
 
 pub mod bloom;
@@ -11,12 +15,17 @@ pub mod bloom;
 struct Opt {
     #[arg(short, env = "BIND", default_value = "[::]:8060")]
     bindhost: SocketAddr,
+
+    /// urls to check for a package, in order of precedence
+    #[arg(required = true)]
+    mirrors: Vec<String>,
 }
 
 async fn handle_conn(
     _: Request<hyper::body::Incoming>,
+    mirrors: Arc<Vec<String>>,
 ) -> Result<Response<Full<Bytes>>, hyper::Error> {
-    Ok(Response::new(Full::new(Bytes::from("meow"))))
+    Ok(Response::new(Full::new(Bytes::from(mirrors[0].clone()))))
 }
 
 #[tokio::main]
@@ -27,15 +36,18 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 
     eprintln!("listening on {}", opt.bindhost);
 
+    let mirrors = Arc::new(opt.mirrors);
+
     loop {
         let (stream, _) = listen.accept().await?;
         let io = TokioIo::new(stream);
 
+        let mirrors = Arc::clone(&mirrors);
+
+        let service = service_fn(move |req| handle_conn(req, Arc::clone(&mirrors)));
+
         tokio::task::spawn(async move {
-            if let Err(e) = http1::Builder::new()
-                .serve_connection(io, service_fn(handle_conn))
-                .await
-            {
+            if let Err(e) = http1::Builder::new().serve_connection(io, service).await {
                 eprintln!("oh no {:?}", e);
             }
         });
