e7188b724e12a54aac6c31bd2f87d756d21738a7
xfnw <xfnw@riseup.net>
2024-02-23 22:01:57 -0500

    cache: add test

--- src/cache.rs
+++ src/cache.rs
@@ -65,4 +65,46 @@ impl<T: Body<Data = Bytes, Error = Error> + Unpin> Body for FanoutBody<T> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::cache::*;
+    use http_body_util::{BodyExt, Full};
+    use hyper::body::Body;
+
+    #[tokio::test]
+    async fn cache_static() {
+        let inp =
+            Full::new(Bytes::from_static(b"you wouldn't download a fox")).map_err(|e| match e {});
+        let cachestore: CacheStore = Arc::new(RwLock::new(std::collections::BTreeMap::new()));
+        let body = FanoutBody {
+            body: inp,
+            uri: "/test".to_string(),
+            buffer: Vec::new(),
+            cachestore: Arc::clone(&cachestore),
+        };
+
+        // FIXME: replace with std::task::Waker::noop once stable
+        // https://github.com/rust-lang/rust/issues/98286
+        let waker = futures::task::noop_waker_ref();
+        let mut cx = std::task::Context::from_waker(&waker);
+        let mut pinned = std::pin::pin!(body);
+
+        let pf = pinned.as_mut().poll_frame(&mut cx);
+        let read = match pf {
+            Poll::Ready(Some(Ok(ref frame))) => frame.data_ref().unwrap(),
+            e => panic!("failed to poll frame: {:?}", e),
+        };
+        assert_eq!(read, &Bytes::from_static(b"you wouldn't download a fox"));
+
+        match pinned.as_mut().poll_frame(&mut cx) {
+            Poll::Ready(None) => (),
+            e => panic!("failed to poll frame: {:?}", e),
+        };
+
+        // wait for FanoutBody to finish caching in the background
+        tokio::task::yield_now().await;
+
+        let res = cachestore.read().await;
+        let res = res.get("/test").unwrap();
+        assert_eq!(res, &Bytes::from_static(b"you wouldn't download a fox"));
+    }
+}
